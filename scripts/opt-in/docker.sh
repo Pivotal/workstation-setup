4b49d87147050fdef04bae1beb8ff1da99d03621
Spencer Whitehead <spencer.whitehead@arrivehealth.com>
2024-07-10 13:52:31 -0400

    have docker install docker-desktop instead of just the cli tools
    docker desktop via --cask also installs the cli tools and adds completions on its own
    
    Co-authored-by: Ed Kerns <blauentag@gmail.com>

--- scripts/opt-in/docker.sh
+++ scripts/opt-in/docker.sh
@@ -2,20 +2,6 @@
 set +e
 
 # Docker
-brew install docker
-brew install docker-compose
-
-echo "To get docker command-line tools, run the docker application"
-
-# Docker completion
-# https://docs.docker.com/desktop/faqs/macfaqs/#how-do-i-install-shell-completion
-etc=/Applications/Docker.app/Contents/Resources/etc
-# Bash
-ln -s $etc/docker.bash-completion "$(brew --prefix)/etc/bash_completion.d/docker"
-ln -s $etc/docker-compose.bash-completion "$(brew --prefix)/etc/bash_completion.d/docker-compose"
-echo '[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion' >> ~/.bash_profile
-# Zsh
-ln -s $etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
-ln -s $etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
+brew install docker --cask
 
 set -e
